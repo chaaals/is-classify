a7f50c8e6245e4f152ff91a407ef504ee80eb59b
chaaals <charlesching0808@gmail.com>
2023-09-27 23:37:06 +0800

    feat: read files and aggregate raw data + implemented unit testing

---
+++ algorithm/classify.py
@@ -0,0 +1,34 @@
+import os
+
+class Classify:
+    def __init__(self,dir: str):
+        self.dir = dir
+        self.raw_data = []
+
+        # checks if the path exists before reading data sets
+        if(os.path.exists(dir)):
+            self.read_data_sets()
+
+    def read_data_sets(self):
+        """
+        Read the files and in a given dir and stores the value in a 2D array
+        """ 
+        files = os.listdir(self.dir)
+        
+        for file in files:
+            path = os.path.join(self.dir, file)
+            file_data = []
+            
+            with open(path, 'r') as f:
+                for line in f:
+                    file_data.append(line.strip())
+
+            self.raw_data.append(file_data)
+
+        return self
+    
+    def get_raw_data(self) -> list:
+        """
+        Getter fn for raw data
+        """
+        return self.raw_data
