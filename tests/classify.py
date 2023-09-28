0d5fab2f761eccd050529fb24ef2e88f89166655
Andre Villanueva <andrevillanueva@Andres-MacBook-Pro.local>
2023-09-28 15:28:41 +0800

    fix: remove tests folder

--- tests/classify.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class ClassifyUnitTest(unittest.TestCase):
-    def test_unitTest(self):
-        self.assertEqual('hello world'.upper(), 'HELLO WORLD')
-
-if __name__ == '__main__':
-    unittest.main()
