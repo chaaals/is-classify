db1f987d7f7eddf8eb8ebae7f8e0def2563fcf3e
saltkid <ojisaltkid@gmail.com>
2023-09-28 15:19:46 +0800

    from os to pathlib + descriptive returns

--- algorithm/classify.py
+++ algorithm/classify.py
@@ -1,13 +1,13 @@
-import os
+from pathlib import Path
 
 class Classify:
-    def __init__(self,dir: str):
+    def __init__(self, dir: Path):
         self.dir = dir
         self.raw_data = [] # array of sets
         self.redundant_data = {}
 
         # checks if the path exists before reading data sets
-        if(os.path.isdir(dir)):
+        if(dir.is_dir()):
             self.__read_data_sets()
         else:
             raise ValueError('Argument must be a valid directory.')
@@ -17,10 +17,10 @@ class Classify:
         Read the files and in a given dir and stores the value in a 2D array.
         Should not be accessible when an instance is created.
         """ 
-        files = os.listdir(self.dir)
+        files = [file for file in self.dir.iterdir() if file.is_file()]
         
         for file in files:
-            path = os.path.join(self.dir, file)
+            path = self.dir.joinpath(file)
             file_data = set()
             
             with open(path, 'r') as f:
@@ -40,13 +40,13 @@ class Classify:
             self.raw_data.append(file_data)
         return self
     
-    def get_raw_data(self) -> list:
+    def get_raw_data(self) -> list[set]:
         """
         Getter fn for raw data
         """
         return self.raw_data
     
-    def get_redundant_data(self) -> dict:
+    def get_redundant_data(self) -> dict[str:int]:
         """
         Getter fn for redundant data
         """
