bd523d0f4c8d4567923844f082c227de4da410a9
Andre Villanueva <andrevillanueva@Andres-MacBook-Pro.local>
2023-10-02 11:57:08 +0800

    chore: duplicate branch of categorize data branch

--- algorithm/classify.py
+++ algorithm/classify.py
@@ -1,10 +1,17 @@
 from pathlib import Path
+import re
 
 class Classify:
     def __init__(self, dir: Path):
         self.dir = dir
         self.raw_data = [] # array of sets
         self.redundant_data = {}
+        self.categorized_data = { # array of lists for categorize_data
+            "name": [],
+            "birthday": [],
+            "email": [],
+            "cell_no": [],
+        }
 
         # checks if the path exists before reading data sets
         if(dir.is_dir()):
@@ -27,7 +34,7 @@ class Classify:
                 for line in f:
                     value = line.strip()
 
-                    # counts the number of times the value has occured
+                    # counts the number of times the value has ocurred
                     if value in file_data:
                         if self.redundant_data.get(value) is not None:
                             self.redundant_data[value] += 1
@@ -51,3 +58,64 @@ class Classify:
         Getter fn for redundant data
         """
         return self.redundant_data
+    
+    def format_name(self, name_str):
+        parts = name_str.split()
+
+        # Check if there are at least two parts (first name and last name)
+        if len(parts) >= 2:
+            first_name = parts[0]
+            last_name = parts[-1]  # Use the last part as the last name
+
+
+        # Check if there are at least three parts (first, middle, last names)
+        if len(parts) >= 3:
+            middle_name = parts[1]
+            middle_initial = middle_name[0]
+            formatted_name = f"{last_name}, {first_name} {middle_initial}."
+        else:
+            formatted_name = f"{last_name}, {first_name}"
+
+        return formatted_name
+    
+        return None
+
+
+    def categorize_data(self):
+        """
+        Categorize the raw data into "name," "email," "birthday," and "cell_no" lists.
+        """
+
+        # Regex for cell numbers, birthdays, and emails
+        cell_no_regex = re.compile(r'^\+63-\d{10}$')
+        birthday_regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+        email_regex = re.compile(r'^.+@.+\..{2,}$')
+
+        for data_set in self.raw_data:
+            for value in data_set:
+                if cell_no_regex.match(value):
+                    self.categorized_data["cell_no"].append(value)
+                elif birthday_regex.match(value):
+                    self.categorized_data["birthday"].append(value)
+                elif email_regex.match(value):
+                    self.categorized_data["email"].append(value)
+                else:
+                    formatted_name = self.format_name(value)
+                    if formatted_name is not None:
+                        self.categorized_data["name"].append(formatted_name)
+
+
+    def get_categorized_data(self):
+        """
+        Getter function for categorized data
+        """
+        return self.categorized_data
+    
+    def print_categorized_data(self):
+        """
+        Print categorized data to the terminal
+        """
+        for category, values in self.categorized_data.items():
+            print(f"{category}:")
+            for value in values:
+                print(f"  {value}")
--- test/test_classify.py
+++ test/test_classify.py
@@ -1,14 +1,17 @@
 import unittest
+import platform
 from pathlib import Path
 from algorithm.classify import Classify
 
 # generates a path for test data directory
 TEST_DATA_DIR = Path(__file__).parent.joinpath("test_data")
+SYSTEM = platform.system()
 classify = Classify(TEST_DATA_DIR)
 
 class ClassifyUnitTest(unittest.TestCase):
-    # should create a list of sets containing clean raw data from txt files
-    def test_read_data(self):
+    # should successfully create a list of sets containing clean raw data from txt files on Windows OS
+    @unittest.skipIf(SYSTEM == "Darwin", "OS != Windows")
+    def test_read_data_windows(self):
         self.assertListEqual(
             classify.get_raw_data(),
         [
@@ -65,6 +68,184 @@ class ClassifyUnitTest(unittest.TestCase):
                 "+63-9182232234",
                 "+63-9906300018",
                 "Destiny Stewart"
+            },
+            {
+                "+63-9519818150",
+                "+63-9174922783",
+                "2007-04-18",
+                "+63-9925222700",
+                "+63-9705067052",
+                "+63-9972566544",
+                "reedlee@example.org",
+                "2010-12-06",
+                "Mario Anderson",
+                "Jordan Yoder",
+                "+63-9278087484",
+                "hcruz@example.com",
+                "Christina Swanson",
+                "+63-9083717947",
+                "Thomas Daniels",
+                "John Williams",
+                "Elizabeth Moore",
+                "Jasmine Myers",
+                "2023-07-03",
+                "2015-02-25",
+                "Andrea Smith",
+                "1986-04-08",
+                "william36@example.org",
+                "+63-9919175150",
+                "2015-07-11",
+            }
+        ])
+
+    # should successfully create a list of sets containing clean raw data from txt files on macOS
+    @unittest.skipIf(SYSTEM == "Windows", "OS != Darwin(macOS)")
+    def test_read_data_macos(self):
+        self.assertListEqual(
+            classify.get_raw_data(),
+        [
+            {
+                "2009-03-28",
+                "1991-07-08",
+                "rjohnson@example.com",
+                "Maria Thornton",
+                "1997-07-12",
+                "Destiny Stewart",
+                "2012-01-18",
+                "Kara Lee MD",
+                "1975-04-23",
+                "Stephanie Estrada",
+                "Brandi Beasley",
+                "1985-12-04",
+                "brenda03@example.org",
+                "Antonio Davila",
+                "april93@example.org",
+                "Stephanie Estrada",
+                "jmendoza@example.com",
+                "schmidtjillian@example.net",
+                "jenniferfreeman@example.com",
+                "estewart@example.com",
+                "+63-9637653230",
+                "+63-9869682919",
+                "+63-9182232234",
+                "+63-9906300018",
+                "Destiny Stewart"
+            },
+            {
+                "+63-9519818150",
+                "+63-9174922783",
+                "2007-04-18",
+                "+63-9925222700",
+                "+63-9705067052",
+                "+63-9972566544",
+                "reedlee@example.org",
+                "2010-12-06",
+                "Mario Anderson",
+                "Jordan Yoder",
+                "+63-9278087484",
+                "hcruz@example.com",
+                "Christina Swanson",
+                "+63-9083717947",
+                "Thomas Daniels",
+                "John Williams",
+                "Elizabeth Moore",
+                "Jasmine Myers",
+                "2023-07-03",
+                "2015-02-25",
+                "Andrea Smith",
+                "1986-04-08",
+                "william36@example.org",
+                "+63-9919175150",
+                "2015-07-11",
+            },
+            {
+                "2016-03-22",
+                "blakebennett@example.org",
+                "2001-11-25",
+                "Paige Obrien",
+                "+63-9529763557",
+                "robertsmichelle@example.net",
+                "michael09@example.org",
+                "2007-09-08",
+                "Kendra Taylor",
+                "+63-9282873428",
+                "Antonio Castillo",
+                "Robert Mcintyre",
+                "+63-9282873428",
+                "2019-02-05",
+                "carlmcintyre@example.org",
+                "Michael Graham",
+                "Amber Thomas",
+                "ushelton@example.org",
+                "1993-08-20",
+                "2019-02-05",
+                "Brooke Huang",
+                "+63-9781474972",
+                "David Wallace",
+                "+63-9143480987",
+                "mathewsjessica@example.net"
+            }
+        ])
+    
+    # should successfully create a list of sets containing clean raw data from txt files on macOS
+    @unittest.skipIf(SYSTEM == "Windows", "OS != Darwin(macOS)")
+    def test_read_data_macos(self):
+        self.assertListEqual(
+            classify.get_raw_data(),
+        [
+            {
+                "2009-03-28",
+                "1991-07-08",
+                "rjohnson@example.com",
+                "Maria Thornton Sam",
+                "1997-07-12",
+                "Destiny Stewart",
+                "2012-01-18",
+                "Kara Lee MD",
+                "1975-04-23",
+                "Stephanie Estrada",
+                "Brandi Beasley",
+                "1985-12-04",
+                "brenda03@example.org",
+                "Antonio Davila",
+                "april93@example.org",
+                "Stephanie Estrada",
+                "jmendoza@example.com",
+                "schmidtjillian@example.net",
+                "jenniferfreeman@example.com",
+                "estewart@example.com",
+                "+63-9637653230",
+                "+63-9869682919",
+                "+63-9182232234",
+                "+63-9906300018",
+                "Destiny Stewart"
+            },
+            {
+                "2016-03-22",
+                "blakebennett@example.org",
+                "2001-11-25",
+                "Paige Obrien",
+                "+63-9529763557",
+                "robertsmichelle@example.net",
+                "michael09@example.org",
+                "2007-09-08",
+                "Kendra Taylor",
+                "+63-9282873428",
+                "Antonio Castillo",
+                "Robert Mcintyre",
+                "+63-9282873428",
+                "2019-02-05",
+                "carlmcintyre@example.org",
+                "Michael Graham",
+                "Amber Thomas",
+                "ushelton@example.org",
+                "1993-08-20",
+                "2019-02-05",
+                "Brooke Huang",
+                "+63-9781474972",
+                "David Wallace",
+                "+63-9143480987",
+                "mathewsjessica@example.net"
             }
         ])
 
@@ -82,5 +263,20 @@ class ClassifyUnitTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             Classify(TEST_DATA_DIR.joinpath('/data_set_471.txt'))
 
+    # should print out the categorized data
+    def test_print_categorized_data(self):
+        classify.categorize_data()
+        print("Categorized Data:")
+        classify.print_categorized_data()
+
+    # should check out if there are duplicates on the categorized data; another validation
+    def test_no_duplicates_in_categorized_data(self):
+        classify.categorize_data()
+        categorized_data = classify.get_categorized_data()
+
+        # Iterate through each category and check for duplicates
+        for category, values in categorized_data.items():
+            self.assertEqual(len(values), len(set(values)), f"Duplicates found in '{category}' category")
+ 
 if __name__ == '__main__':
     unittest.main()
