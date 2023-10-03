edeb24aef64b71ef82f774fc83e77467233a536f
Otentackles <97921102+Otentackles@users.noreply.github.com>
2023-10-03 18:33:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from algorithm.classify import Classify
+from datetime import datetime
 
 def print_to_terminal(classify: Classify):
     # print raw data by file
@@ -22,27 +23,19 @@ def print_to_terminal(classify: Classify):
 
     for category, dataset in classify.get_categorized_data().items():
         print(f"{category}:")
-        print(dataset)
+        print('\n'.join(dataset))  # Add a newline after each data point
         print('\n')
 
-"def write_to_file(classify: Classify):
- "   pass
-
-"if __name__ == '__main__':
- "   DATA_DIR = Path(__file__).parent / 'data'
-  "  classify = Classify(DATA_DIR)
-
-   " print_to_terminal(classify)
-    "write_to_file(classify)
-
 def write_to_file(classify: Classify):
-    output_file_path = Path(__file__).parent / 'DATABASE.txt'
+    output_dir = Path(__file__).parent / 'data' / 'categorized_database'
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_file_path = output_dir / 'DATABASE.txt'  # Output file path named DATABASE.txt
     with open(output_file_path, 'w') as output_file:
-        print_to_txt = classify.get_categorized_data() #hopefully gumana gomenasaiiii
+        print_to_txt = classify.get_categorized_data()
         for category, data_set in print_to_txt.items():
-            output_file.write(f"{category}:\n")
+            output_file.write(f"{category} ({datetime.now().strftime('%Y-%m-%d %H:%M:%S')}): {len(data_set)}\n")
             for data in data_set:
-                output_file.write(f"     {data}\n")
+                output_file.write(f"\t{data}\n")  # Use '\t' instead of spaces
 
 if __name__ == '__main__':
     DATA_DIR = Path(__file__).parent / 'data'
