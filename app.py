c9f0b8905cbc6a1c85840087806340299f333587
chaaals <charlesching0808@gmail.com>
2023-10-09 21:01:40 +0800

    chore: exported categorized data with new changes

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     DATA_DIR = Path(__file__).parent / 'data'
     classify = Classify(DATA_DIR)
     classify.categorize_data().print_categorized_data()
+    classify.export()
